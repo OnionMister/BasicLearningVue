fa654ca0b763664466612b9c18ac8d9e0ad7cf0e
Carpenterworm <sunyst>
2020-04-19 13:06:27 +0800

    master

--- js/api.js
+++ js/api.js
@@ -12,7 +12,7 @@ app.all('*', function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,myToken');
     next();
 })
 
@@ -72,16 +72,34 @@ app.get('/asData1', (req, res) => {
 app.get('/asData2/:id', (req, res) => {
     res.send(`Axios的Restful形式URL的Get传参：${req.params.id}`);
 })
+// axios的get传递参数---params
+app.get('/asData3', (req, res) => {
+    res.send(`Axios通过Get传递params参数：${req.query.id}`)
+})
 //---------------------------------------------------------------
 // axios的delete传递参数---restful形式的URL传参
 app.delete('/asDataDel/:id/:name', (req, res) => {
     res.send(`Axios的Delete传参,restful形式的URL传多参：${req.params.id}----${req.params.name}`)
 })
+//---------------------------------------------------------------
+// axios的Post传参---默认Json
+app.post('/asDataPost1', (req, res) => {
+    res.send(`Axios通过Post传来的默认Json数据：${req.body.id}----${req.body.name}`);
+})
+// axios的Post传参---表单格式
+app.post('/asDataPost2', (req, res) => {
+    res.send(`Axios通过Post传来的默认表单数据：${req.body.sendType}----${req.body.dataType}----${req.body.way}`);
+})
+//---------------------------------------------------------------
+// axios的put传参---默认Json
+app.put('/asDataPut/:id', (req, res) => {
+    res.send(`Axios通过Put传来的默认Json数据：${req.params.id}----${req.body.sendType}----${req.body.name}`)
+})
+// Axios配置过基准URL地址的post请求
+app.post('/asDflDataPost', (req, res) => {
+    res.json(req.body);
+})
 // 启动监听
 app.listen(3101, () => {
     console.log('ServiceTurnedOn')
-})
-// axios的get传递参数---params
-app.get('/asData3', (req, res) => {
-    res.send(`Axios通过Get传递params参数：${req.query.id}`)
 })
